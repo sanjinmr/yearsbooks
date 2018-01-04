8000e0c6c185836230b965584a6e1844255f90cd
dongmingxin <dongmingxin@yinyueke.com>
2018-01-04 08:36:22 +0800

    完成底部导航栏布局

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,4 @@
-// pages/addPage/addPag.js
+// pages/home/home.js
 Page({
 
   /**
@@ -12,55 +12,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.debug("onLoad.");
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    console.debug("onReady.");
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    console.debug("onShow.");
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    console.debug("onHide.");
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    console.debug("onUnload.");
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    console.debug("onPullDownRefresh");
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    console.debug("onReachBottom");
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    console.debug("onShareAppMessage");
+  
   }
 })
